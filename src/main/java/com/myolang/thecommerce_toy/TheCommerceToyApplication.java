6d27ed1218a2c50728a3d39ee539d21392ce0bc8
Myolang <myohanmyolang@gmail.com>
2024-04-23 20:53:42 +0900

    init Commit

---
+++ src/main/java/com/myolang/thecommerce_toy/TheCommerceToyApplication.java
@@ -0,0 +1,13 @@
+package com.myolang.thecommerce_toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCommerceToyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TheCommerceToyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/myolang/thecommerce_toy/TheCommerceToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myolang.thecommerce_toy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceToyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
