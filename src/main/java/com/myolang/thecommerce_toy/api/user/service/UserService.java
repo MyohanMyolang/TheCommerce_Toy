7b92a01107d99e6490b50d82e1a962b55d9be4d0
Myolang <myohanmyolang@gmail.com>
2024-04-26 19:07:25 +0900

    Bugfix: Update할 때 password 암호화가 빠져있던 상황 수정

--- src/main/java/com/myolang/thecommerce_toy/api/user/service/UserService.java
+++ src/main/java/com/myolang/thecommerce_toy/api/user/service/UserService.java
@@ -7,10 +7,13 @@ import com.myolang.thecommerce_toy.domain.member.model.Member;
 import com.myolang.thecommerce_toy.domain.member.repository.IMemberRepository;
 import com.myolang.thecommerce_toy.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +21,7 @@ public class UserService {
 
   private final MemberService memberService;
   private final IMemberRepository memberRepository;
+  private final PasswordEncoder passwordEncoder;
 
   @Transactional
   public void updateUserInfo(String memberId, UpdateUserInfoRequest request) {
@@ -25,12 +29,21 @@ public class UserService {
 
     request.getNickname().ifPresent(memberService::isHasNickname);
 
+    Function<Optional<String>, String> ifPresentPasswordThenEncode = (password) -> {
+      String result = member.getPassword();
+
+      if (password.isPresent())
+        result = passwordEncoder.encode(password.get());
+
+      return result;
+    };
+
     Member updatedMember = member.toBuilder()
       .nickname(request.getNickname().orElse(member.getNickname()))
       .name(request.getName().orElse(member.getName()))
       .phoneNumber(request.getPhoneNumber().orElse(member.getPhoneNumber()))
       .email(request.getEmail().orElse(member.getEmail()))
-      .password(request.getPassword().orElse(member.getPassword()))
+      .password(ifPresentPasswordThenEncode.apply(request.getPassword()))
       .build();
 
     memberService.saveMember(updatedMember);
